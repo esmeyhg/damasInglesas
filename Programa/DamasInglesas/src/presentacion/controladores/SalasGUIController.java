61a1d8e035eaa035edf3e1438278e7e863945426
Esmeralda Yamileth Hernández González <esmeyhg@gmail.com>
2017-12-27 01:50:53 -0600

    Temporizador actualizado
    Temporizador funcionando correctamente, tiempo definido = 30 minutos de duración del juego

--- Programa/DamasInglesas/src/presentacion/controladores/SalasGUIController.java
+++ Programa/DamasInglesas/src/presentacion/controladores/SalasGUIController.java
@@ -1,19 +1,35 @@
 package presentacion.controladores;
 
 import io.socket.client.Socket;
+import java.awt.Color;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -25,6 +41,19 @@ public class SalasGUIController implements Initializable {
     private Parent root;
     private Stage stage;
     Socket socket;
+    
+    final String SON_CHECKERS = "/presentacion/recursos/Checkers.mp3";
+    final String SON_FINJUEGO = "/presentacion/recursos/GameOver.mp3";
+   
+    private Label lblValor;
+    private Slider sldValor;
+    private Button acciones;
+    
+    private Timeline temporizador;
+    
+    int tiempoFaltante;
+    private MediaPlayer mpCheckers;
+    private MediaPlayer mpFinJuego;
 
     ResourceBundle resource = ResourceBundle.getBundle("lenguajes.idioma");
     
@@ -39,25 +68,136 @@ public class SalasGUIController implements Initializable {
      */
     @FXML
     private void abrirTablero(ActionEvent event) {
-        if (event.getSource() == iniciarBT) {
-            try {
-                stage = new Stage();
-                root = FXMLLoader.load(getClass().getResource
-                        ("/presentacion/Tablero.fxml"), resource);
-                stage.setScene(new Scene(root));
-                stage.setTitle("Puntajes");
-                stage.showAndWait();
-
-            } catch (IOException ex) {
-                Logger.getLogger(InicialController.class.getName())
-                        .log(Level.SEVERE, null, ex);
-            }
-        } else {
-            stage = (Stage) iniciarBT.getScene().getWindow();
-            stage.close();
+      if (event.getSource() == iniciarBT) {
+        try {
+          stage = new Stage();
+          BorderPane ventanaTablero = FXMLLoader.load(getClass().getResource
+            ("/presentacion/Tablero.fxml"), resource);
+          AnchorPane temporizadorPane = new AnchorPane();
+          iniciarJuego();
+          VBox vbRaiz = controlInterfaz();
+          sldValor.disableProperty().bind(
+				temporizador.statusProperty().isEqualTo(Animation.Status.RUNNING));
+          ajustaLabel();
+          temporizadorPane.getChildren().addAll(vbRaiz);
+          
+          ventanaTablero.setRight(vbRaiz);
+          BorderPane.setAlignment(temporizadorPane, Pos.CENTER_RIGHT);
+          
+          stage.setScene(new Scene(ventanaTablero));
+          stage.setTitle("DamasInglesas");
+          stage.showAndWait();
+        } catch (IOException ex) {
+          Logger.getLogger(SalasGUIController.class.getName())
+              .log(Level.SEVERE, null, ex);
         }
+      } else {
+        stage = (Stage) iniciarBT.getScene().getWindow();
+        stage.close();
+      }
     }
-
+  
+    /**
+     * Método que permite visibilizar el temporizador y los sonidos 
+     * en determinado tiempo del juego
+     */
+    public void iniciarJuego() {
+      temporizador = new Timeline();
+      temporizador.setCycleCount(Animation.INDEFINITE);
+      
+      String archivoCheckers = TableroController.class.getResource(SON_CHECKERS).toString();
+      String archivoFinJuego = TableroController.class.getResource(SON_FINJUEGO).toString();
+      
+      mpCheckers = new MediaPlayer(new Media(archivoCheckers));
+      mpCheckers.setOnEndOfMedia(() -> {
+        mpCheckers.stop();
+      });
+      mpFinJuego = new MediaPlayer(new Media(archivoFinJuego));
+    }
+    
+    /**
+     * Método que determina el tiempo que durará el juego antes de perder 
+     * tiene 30 minutos definidos, pero aquí se puede modificar
+     * @return regresa el VBox para la modificación de los valores del
+     * temporizador
+     */
+    private VBox controlInterfaz () {
+      lblValor = new Label();
+      sldValor = new Slider(0,30 * 60, (30 * 60));
+      acciones = new Button(resource.getString("partidanueva"));
+      
+      lblValor.setFont(Font.font("Niagara Engraved", FontWeight.BOLD,80));
+      lblValor.setTextFill(Paint.valueOf("WHITE"));
+      sldValor.valueProperty().addListener(l -> ajustaLabel());
+      VBox vbRaiz = new VBox (50, lblValor, sldValor, acciones);
+      vbRaiz.setAlignment(Pos.TOP_CENTER);
+      
+      acciones.setOnAction(e -> accionTemporizador());
+      return vbRaiz;
+    }
+    
+    /**
+     * Método que efectúa los cambios de tiempo y realiza la cuenta
+     * regresiva de acuerdo a los eventos registrados, en este caso,
+     * si el usuario presiona iniciar, el temporizador comienza, y si
+     * presiona pausar juego, el temporizador se detiene permitiendo
+     * reaunudar el temporizador
+     */
+    private void accionTemporizador () {
+      if (acciones.getText().equals(resource.getString("partidanueva"))) {
+        acciones.setText(resource.getString("pausar"));
+        int segundos = sldValor.valueProperty().intValue();
+        final KeyFrame frameSegundos = new KeyFrame(Duration.seconds(1),
+            e -> actualizaValores());
+        temporizador.getKeyFrames().setAll(frameSegundos);
+        temporizador.playFrom(Duration.seconds(segundos));
+        tiempoFaltante = segundos;
+      } else {
+        acciones.setText(resource.getString("partidanueva"));
+        temporizador.stop();
+      }
+    }
+    
+    /**
+     * Actualización de la etiqueta del temporizador
+     */
+    private void ajustaLabel() {
+      if (sldValor.isDisable() == false) {
+        int segundos = sldValor.valueProperty().intValue();
+        actualizaLabelTiempo(segundos);
+      }
+    }
+    
+    /**
+     * Actualización de los valores del temporizador 
+     * de acuerdo a las acciones realizadas, lleva también
+     * un control del tiempo faltante para terminar la cuenta
+     * regresiva.
+     */
+    private void actualizaValores () {
+      if (tiempoFaltante < 1) {
+        temporizador.stop();
+        acciones.setText(resource.getString("partidanueva"));
+        mpFinJuego.stop();
+        mpFinJuego.play();
+        return;
+      }
+      tiempoFaltante--;
+      mpCheckers.play();
+      actualizaLabelTiempo(tiempoFaltante);
+      sldValor.setValue(tiempoFaltante);
+    }
+    
+    /**
+     * Actualiza la etiqueta poniendo el formato adecuado 
+     * según los segundos o minutos transcurridos.
+     * @param segundos tiempo 
+     */
+    private void actualizaLabelTiempo (int segundos) {
+      String txtMinutos = String.format("%02d", segundos / 60);
+      String txtSegundos = String.format("%02d", segundos % 60);
+      lblValor.setText(txtMinutos + ":" + txtSegundos);
+    }
 
   /**
    * Initializes the controller class.
--- Programa/DamasInglesas/src/presentacion/controladores/TableroController.java
+++ Programa/DamasInglesas/src/presentacion/controladores/TableroController.java
@@ -10,35 +10,18 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.HPos;
-import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-import org.json.JSONObject;
 
 /**
  *
@@ -54,100 +37,7 @@ public class TableroController implements Initializable {
     @FXML private MenuItem pausar;
     @FXML private MenuItem iniciar;
     @FXML private GridPane gripTablero;
-    @FXML private VBox vbRaiz;
-    @FXML private BorderPane modulosTablero;
-    
-   
-    final String SON_CHECKERS = "/presentacion/recursos/Checkers.mp3";
-    final String SON_FINJUEGO = "/presentacion/recursos/GameOver.mp3";
-    
-    final String INICIAR = "Iniciar juego";
-    final String PAUSAR = "Pausar juego";
-    
-    private Label lblValor;
-    private Slider sldValor;
-    private Button acciones;
-    
-    private Timeline temporizador;
-    
-    int tiempoFaltante;
-    private MediaPlayer mpCheckers;
-    private MediaPlayer mpFinJuego;
-    
-    public void iniciarJuego() {
-      temporizador = new Timeline();
-      temporizador.setCycleCount(Animation.INDEFINITE);
-      
-      String archivoCheckers = TableroController.class.getResource(SON_CHECKERS).toString();
-      String archivoFinJuego = TableroController.class.getResource(SON_FINJUEGO).toString();
-      
-      mpCheckers = new MediaPlayer(new Media(archivoCheckers));
-      mpCheckers.setOnEndOfMedia(() -> {
-        mpCheckers.stop();
-      });
-      mpFinJuego = new MediaPlayer(new Media(archivoFinJuego));
-    }
-    
-    private VBox controlInterfaz () {
-      lblValor = new Label();
-      sldValor = new Slider(0,30 * 60, (30 * 60) / 2);
-      acciones = new Button(INICIAR);
-      
-      lblValor.setFont(Font.font("Niagara Engraved", FontWeight.BOLD,50));
-      sldValor.valueProperty().addListener(l -> ajustaLabel());
-      vbRaiz = new VBox (50, lblValor, sldValor, acciones);
-      vbRaiz.setAlignment(Pos.TOP_CENTER);
-      
-      acciones.setOnAction(e -> accionTemporizador());
-      return vbRaiz;
-    }
-    
-    private void accionTemporizador () {
-      if (acciones.getText().equals(INICIAR)) {
-        acciones.setText(PAUSAR);
-        int segundos = sldValor.valueProperty().intValue();
-        final KeyFrame frameSegundos = new KeyFrame(Duration.seconds(1),
-            e -> actualizaValores());
-        temporizador.getKeyFrames().setAll(frameSegundos);
-        temporizador.playFrom(Duration.seconds(segundos));
-        tiempoFaltante = segundos;
-      } else {
-        acciones.setText(INICIAR);
-        temporizador.stop();
-      }
-    }
-    
-    private void ajustaLabel() {
-      if (sldValor.isDisable() == false) {
-        int segundos = sldValor.valueProperty().intValue();
-        actualizaLabelTiempo(segundos);
-      }
-    }
-    
-    private void actualizaValores () {
-      if (tiempoFaltante < 1) {
-        temporizador.stop();
-        acciones.setText(INICIAR);
-        mpFinJuego.stop();
-        mpFinJuego.play();
-        return;
-      }
-      tiempoFaltante--;
-      mpCheckers.play();
-      actualizaLabelTiempo(tiempoFaltante);
-      sldValor.setValue(tiempoFaltante);
-    }
-    
-    private void actualizaLabelTiempo (int segundos) {
-      String txtMinutos = String.format("%02d", segundos / 60);
-      String txtSegundos = String.format("%02d", segundos % 60);
-      lblValor.setText(txtMinutos + ":" + txtSegundos);
-    }
-    
-    
-    
-    
-    
+
     /**
      * Conecta con el servidor Server.js a traves del puerto
      * 7000 y la red 192.168.43.239
@@ -195,11 +85,6 @@ public class TableroController implements Initializable {
       this.resource = rb;
       try {
         conectarServidor();
-        iniciarJuego();
-        vbRaiz = controlInterfaz();
-        sldValor.disableProperty().bind(
-            temporizador.statusProperty().isEqualTo(Animation.Status.RUNNING));
-		ajustaLabel();
         for (int i = 0; i < 8; i++) {
           for (int j = 0; j < 8; j++) {
             Button casilla = new Button ("");
@@ -215,9 +100,6 @@ public class TableroController implements Initializable {
             GridPane.setHalignment(casilla, HPos.CENTER);
           }
         }
-
-
-        
       } catch (URISyntaxException ex) {
         Logger.getLogger(TableroController.class.getName()).log(Level.SEVERE, null, ex);
       }  
